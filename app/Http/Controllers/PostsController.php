bc1d12faebc1004de960809646c39f8d3a49e1ad
Peter Mackay <project2501@gmail.com>
2019-09-20 16:10:48 +0900

    created post model, updated query to use model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Http\Controllers;
 
+use DB;
+use App\Post;
+
 class PostsController{
   public function show($slug){
     // test the db connection
@@ -9,10 +12,13 @@ class PostsController{
     //   echo "conncted sucessfully to database ".\DB::connection()->getDatabaseName();
     // }
 
-    $post = \DB::table('posts')->where('slug', $slug)->first();
+    // Laravel Query Builder style query
+    // $post = DB::table('posts')->where('slug', $slug)->first();
+
     // use dd to log out the result
     // dd($post);
-    return view('post',['post'=> $post]);
+
+    return view('post',['post'=> Post::where('slug', $slug)->firstOrFail()]);
   }
 
   public function index(){
